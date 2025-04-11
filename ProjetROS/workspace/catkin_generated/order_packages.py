af5046da5b8fcaff1fc8e244f96b1686f656cd91
Mouafo Bill <151171186+stevendemayo@users.noreply.github.com>
2025-04-11 11:26:59 +0200

    Delete ProjetROS directory

--- ProjetROS/workspace/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ros/ProjetROS/workspace/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ros/ProjetROS/workspace/devel;/home/ros/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ros/ProjetROS/workspace/devel;/home/ros/catkin_ws/devel;/opt/ros/noetic' != '' else []
